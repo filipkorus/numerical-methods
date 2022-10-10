a4889db4dc14995650dfc414c0e68a158931ffd6
Filip Korus <filipkorus123@gmail.com>
2022-10-10 19:51:44 +0200

    bug fixed

--- lab1/z1_2.m
+++ lab1/z1_2.m
@@ -1,18 +1,11 @@
 % zadanie 1.2 (**)
 clear all; close all;
 
-a = 1; b = 10; c = 1; % input values
+a = 9; b = 0; c = 4; % input values
 fprintf('%dx^2 + %dx + %d = 0\n\n', a, b, c);
 
 delta = b^2 - 4*a*c;
 
-if delta < 0
-    disp('This quadratic equation has no solutions!');
-elseif delta == 0
-    x1 = -b / (2 * a);
-    fprintf('x1 = %f', x1);
-else
-    x1 = (-b + sqrt(delta)) / (2 * a);
-    x2 = (-b - sqrt(delta)) / (2 * a);
-    fprintf('x1 = %f\nx2 = %f\n\n', x1, x2);
-end;
+x1 = (-b + sqrt(delta)) / (2 * a);
+x2 = (-b - sqrt(delta)) / (2 * a);
+fprintf('x1 = %f%+fj\nx2 = %f%+fj\n\n', real(x1), imag(x1), real(x2), imag(x2));
